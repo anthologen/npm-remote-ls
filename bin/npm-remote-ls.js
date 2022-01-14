2dbfd5a84b3fad947a2ade461223479eba6264f9
anthologen <anthologen@gmail.com>
2022-01-13 23:41:02 -0500

    lint with standard

--- bin/npm-remote-ls.js
+++ bin/npm-remote-ls.js
@@ -1,79 +1,79 @@
 #!/usr/bin/env node
-import Yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import registryUrl from 'registry-url';
+import Yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import registryUrl from 'registry-url'
 
-const argv = Yargs(hideBin(process.argv))
-    .usage('$0 <pkg-name> [options]')
-    .command('$0 <pkg-name>', 'list package dependencies', (yargs) => {
-      yargs.positional('pkgName', {
-        describe: 'package name',
-      })
-    }, (argv) => { list_dependencies(argv) })
-    .option('n', {
-      alias: 'name',
-      description: 'package name'
-    })
-    .version(false)
-    .option('v', {
-      alias: 'version',
-      description: 'package version',
-      default: 'latest'
-    })
-    .option('e', {
-      alias: 'verbose',
-      description: 'enable verbose logging',
-      default: false,
-      boolean: true
-    })
-    .option('d', {
-      alias: 'development',
-      description: 'show development dependencies',
-      default: true,
-      boolean: true
-    })
-    .option('o', {
-      alias: 'optional',
-      description: 'show optional dependencies',
-      default: true,
-      boolean: true
-    })
-    .option('p', {
-      alias: 'peer',
-      description: 'show peer dependencies',
-      default: false,
-      boolean: true
-    })
-    .option('r', {
-      alias: 'registry',
-      description: 'set an alternative registry url',
-      default: registryUrl
-    })
-    .option('f', {
-      alias: 'flatten',
-      description: 'return flat representation of dependencies',
-      default: false,
-      boolean: true
-    })
-    .parse();
+import { ls } from '../lib/index.js'
+import treeify from 'treeify'
+import spinner from 'char-spinner'
+import npa from 'npm-package-arg'
 
-import { ls } from '../lib/index.js';
-import treeify from 'treeify';
-import spinner from 'char-spinner';
-import npa from 'npm-package-arg';
+Yargs(hideBin(process.argv))
+  .usage('$0 <pkg-name> [options]')
+  .command('$0 <pkg-name>', 'list package dependencies', (yargs) => {
+    yargs.positional('pkgName', {
+      describe: 'package name'
+    })
+  }, (argv) => { listDependencies(argv) })
+  .option('n', {
+    alias: 'name',
+    description: 'package name'
+  })
+  .version(false)
+  .option('v', {
+    alias: 'version',
+    description: 'package version',
+    default: 'latest'
+  })
+  .option('e', {
+    alias: 'verbose',
+    description: 'enable verbose logging',
+    default: false,
+    boolean: true
+  })
+  .option('d', {
+    alias: 'development',
+    description: 'show development dependencies',
+    default: true,
+    boolean: true
+  })
+  .option('o', {
+    alias: 'optional',
+    description: 'show optional dependencies',
+    default: true,
+    boolean: true
+  })
+  .option('p', {
+    alias: 'peer',
+    description: 'show peer dependencies',
+    default: false,
+    boolean: true
+  })
+  .option('r', {
+    alias: 'registry',
+    description: 'set an alternative registry url',
+    default: registryUrl
+  })
+  .option('f', {
+    alias: 'flatten',
+    description: 'return flat representation of dependencies',
+    default: false,
+    boolean: true
+  })
+  .parse()
 
-function list_dependencies(argv) {
-  spinner();
-  const parsed = npa(argv.pkgName);
+function listDependencies (argv) {
+  spinner()
+  const parsed = npa(argv.pkgName)
   const opts = {
     verbose: argv.verbose,
     development: argv.development,
     optional: argv.optional,
     peer: argv.peer,
     registry: argv.registry
-  };
+  }
   ls(parsed.name, parsed.rawSpec || argv.version, argv.flatten, opts, obj => {
     if (Array.isArray(obj)) console.log(obj)
     else console.log(treeify.asTree(obj))
-  });
+  })
 }
--- test/remote-ls-test.js
+++ test/remote-ls-test.js
@@ -1,18 +1,18 @@
-import {test} from 'tap';
-import nock from 'nock';
-import fs from 'fs';
-import RemoteLS from '../lib/remote-ls.js';
-import chai from 'chai';
-chai.should();
+import { test } from 'tap'
+import nock from 'nock'
+import fs from 'fs'
+import RemoteLS from '../lib/remote-ls.js'
+import chai from 'chai'
+chai.should()
 
 test('RemoteLS', t => {
   t.test('guessVersion', t => {
     t.test('should handle an exact version being provided', t => {
-      const versionString = '1.0.0';
+      const versionString = '1.0.0'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.equal(
         ls._guessVersion(versionString, packageJson),
@@ -22,11 +22,11 @@ test('RemoteLS', t => {
     })
 
     t.test('should handle a complex version being provided', t => {
-      const versionString = '*';
+      const versionString = '*'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.equal(
         ls._guessVersion(versionString, packageJson),
@@ -36,11 +36,11 @@ test('RemoteLS', t => {
     })
 
     t.test('should raise an exception if version cannot be found', t => {
-      const versionString = '9.0.0';
+      const versionString = '9.0.0'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.throws(
         () => {
@@ -52,11 +52,11 @@ test('RemoteLS', t => {
     })
 
     t.test('should raise an exception if version cannot be found', t => {
-      const versionString = '9.0.0';
+      const versionString = '9.0.0'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.throws(
         () => {
@@ -68,11 +68,11 @@ test('RemoteLS', t => {
     })
 
     t.test('should handle "latest" being provided as version', t => {
-      const versionString = 'latest';
+      const versionString = 'latest'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.equal(
         ls._guessVersion(versionString, packageJson),
@@ -82,11 +82,11 @@ test('RemoteLS', t => {
     })
 
     t.test('should return dist-tags.latest when * wanted and package has only prerelease versions', t => {
-      const versionString = '*';
+      const versionString = '*'
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/angular-core.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.equal(
         ls._guessVersion(versionString, packageJson),
@@ -102,17 +102,17 @@ test('RemoteLS', t => {
     t.test('should push appropriate dependencies to queue', t => {
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
+      )
       const ls = new RemoteLS({
         queue: {
-          pause() {},
-          push({name, version}) {
+          pause () {},
+          push ({ name, version }) {
             t.equal(name, 'abbrev')
             t.equal(version, '1')
             t.end()
           }
         }
-      });
+      })
 
       ls._walkDependencies({
         name: 'nopt',
@@ -124,17 +124,17 @@ test('RemoteLS', t => {
     t.test('should push devDependencies to queue', t => {
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
+      )
       const ls = new RemoteLS({
         queue: {
-          pause() {},
-          push({name, version}) {
+          pause () {},
+          push ({ name, version }) {
             t.equal(name, 'tap')
             t.equal(version, '1.0.0')
             t.end()
           }
         }
-      });
+      })
 
       ls._walkDependencies({
         name: 'nopt',
@@ -146,8 +146,8 @@ test('RemoteLS', t => {
     t.test('should not raise an exception if package has no dependencies', t => {
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/abbrev.json').toString()
-      );
-      const ls = new RemoteLS();
+      )
+      const ls = new RemoteLS()
 
       t.doesNotThrow(() => {
         ls._walkDependencies({
@@ -163,19 +163,19 @@ test('RemoteLS', t => {
     t.test('should not walk dependency if dependency has already been observed', t => {
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/nopt.json').toString()
-      );
+      )
       const ls = new RemoteLS({
         flat: {
           'nopt@1.0.0': true
         },
         queue: {
-          pause() {},
-          push(obj) {
+          pause () {},
+          push (obj) {
             t.fail('should not walk dependency')
             t.end()
           }
         }
-      });
+      })
 
       ls._walkDependencies({
         name: 'nopt',
@@ -189,18 +189,18 @@ test('RemoteLS', t => {
     t.test('should push peerDependencies to queue', t => {
       const packageJson = JSON.parse(
         fs.readFileSync('./test/fixtures/angular-core.json').toString()
-      );
+      )
       const ls = new RemoteLS({
         peer: true,
         queue: {
-          pause() {},
-          push({name, version}) {
+          pause () {},
+          push ({ name, version }) {
             t.equal(name, 'rxjs')
             t.equal(version, '5.0.0-beta.6')
             t.end()
           }
         }
-      });
+      })
 
       ls._walkDependencies({
         name: 'angular',
@@ -215,63 +215,63 @@ test('RemoteLS', t => {
   t.test('ls', t => {
     t.test('handles a 404 and prints an appropriate message', t => {
       nock('https://skimdb.npmjs.com')
-          .get('/registry/request')
-          .reply(404)
+        .get('/registry/request')
+        .reply(404)
       const ls = new RemoteLS({
         registry: 'https://skimdb.npmjs.com/registry/',
         logger: {
-          log(msg) {
+          log (msg) {
             t.match(msg, /status = 404/)
             t.end()
           }
         }
-      });
+      })
 
       ls.ls('request', '*', () => {})
     })
 
     t.test('defaults to appropriate registry URL', t => {
       nock('https://registry.npmjs.org')
-          .get('/request')
-          .reply(404)
+        .get('/request')
+        .reply(404)
       const ls = new RemoteLS({
         logger: {
-          log(msg) {
+          log (msg) {
             t.match(msg, /status = 404/)
             t.end()
           }
         }
-      });
+      })
 
       ls.ls('request', '*', () => {})
     })
 
     t.test('happy path works as expected', t => {
       const request = nock('https://registry.npmjs.org')
-          .get('/request')
-          .reply(200, {
-            name: 'request',
-            versions: {
-              '0.0.1': {
-                dependencies: {
-                  lodash: '0.0.2'
-                }
+        .get('/request')
+        .reply(200, {
+          name: 'request',
+          versions: {
+            '0.0.1': {
+              dependencies: {
+                lodash: '0.0.2'
               }
             }
-          });
+          }
+        })
       const lodash = nock('https://registry.npmjs.org')
-          .get('/lodash')
-          .reply(200, {
-            name: 'lodash',
-            versions: {
-              '0.0.2': {
-                dependencies: {}
-              }
+        .get('/lodash')
+        .reply(200, {
+          name: 'lodash',
+          versions: {
+            '0.0.2': {
+              dependencies: {}
             }
-          });
-      const ls = new RemoteLS();
+          }
+        })
+      const ls = new RemoteLS()
 
-      ls.ls('request', '*', ({should}) => {
+      ls.ls('request', '*', ({ should }) => {
         should.deep.equal({ 'request@0.0.1': { 'lodash@0.0.2': {} } })
         request.done()
         lodash.done()
@@ -281,36 +281,36 @@ test('RemoteLS', t => {
 
     t.test('supports scoped packages', t => {
       const storybook = nock('https://registry.npmjs.org')
-          .get('/@kadira%2fstorybook')
-          .reply(200, {
-            name: '@kadira/storybook',
-            versions: {
-              '1.30.0': {
-                dependencies: { '@kadira/storybook-core': '1.27.0' }
-              }
+        .get('/@kadira%2fstorybook')
+        .reply(200, {
+          name: '@kadira/storybook',
+          versions: {
+            '1.30.0': {
+              dependencies: { '@kadira/storybook-core': '1.27.0' }
             }
-          });
+          }
+        })
       const storybook404 = nock('https://registry.npmjs.org')
-          .get('/@kadira/storybook')
-          .reply(404, {
-            error: 'Not found'
-          });
+        .get('/@kadira/storybook')
+        .reply(404, {
+          error: 'Not found'
+        })
       const core = nock('https://registry.npmjs.org')
-          .get('/@kadira%2fstorybook-core')
-          .reply(200, {
-            name: '@kadira/storybook-core',
-            versions: {
-              '1.27.0': { dependencies: {} }
-            }
-          });
+        .get('/@kadira%2fstorybook-core')
+        .reply(200, {
+          name: '@kadira/storybook-core',
+          versions: {
+            '1.27.0': { dependencies: {} }
+          }
+        })
       const core404 = nock('https://registry.npmjs.org')
-          .get('/@kadira/storybook-core')
-          .reply(404, {
-            error: 'Not found'
-          });
-      const ls = new RemoteLS();
+        .get('/@kadira/storybook-core')
+        .reply(404, {
+          error: 'Not found'
+        })
+      const ls = new RemoteLS()
 
-      ls.ls('@kadira/storybook', '*', ({should}) => {
+      ls.ls('@kadira/storybook', '*', ({ should }) => {
         should.deep.equal({ '@kadira/storybook@1.30.0': { '@kadira/storybook-core@1.27.0': {} } })
         storybook.done()
         core.done()
