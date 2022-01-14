2955fbae0e2beafc31d9135b0c01d743f2faef69
anthologen <anthologen@gmail.com>
2022-01-13 23:30:51 -0500

    parameterize config

--- bin/npm-remote-ls.js
+++ bin/npm-remote-ls.js
@@ -57,22 +57,22 @@ const argv = Yargs(hideBin(process.argv))
     })
     .parse();
 
-import { ls, config } from '../lib/index.js';
+import { ls } from '../lib/index.js';
 import treeify from 'treeify';
 import spinner from 'char-spinner';
 import npa from 'npm-package-arg';
-config({
-  verbose: argv.verbose,
-  development: argv.development,
-  optional: argv.optional,
-  peer: argv.peer,
-  registry: argv.registry
-});
 
 function list_dependencies(argv) {
   spinner();
   const parsed = npa(argv.pkgName);
-  ls(parsed.name, parsed.rawSpec || argv.version, argv.flatten, obj => {
+  const opts = {
+    verbose: argv.verbose,
+    development: argv.development,
+    optional: argv.optional,
+    peer: argv.peer,
+    registry: argv.registry
+  };
+  ls(parsed.name, parsed.rawSpec || argv.version, argv.flatten, opts, obj => {
     if (Array.isArray(obj)) console.log(obj)
     else console.log(treeify.asTree(obj))
   });
